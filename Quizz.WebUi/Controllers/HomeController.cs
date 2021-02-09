f85ceb7ec50b26d2c42475bf39b53133bf54fc47
Loïs Silvin <loissilvin.pro@gmail.com>
2021-02-10 00:30:01 +0100

    conflit fix

--- Quizz.WebUi/Controllers/HomeController.cs
+++ Quizz.WebUi/Controllers/HomeController.cs
@@ -1,30 +1,98 @@
-﻿using System;
+﻿using Quizz.Core.Logic;
+using Quizz.Core.Models;
+using Quizz.Core.ViewModels;
+using Quizz.DataAccess.SQL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+
+
 namespace Quizz.WebUi.Controllers
 {
-    public class HomeController : Controller
-    {
-        public ActionResult Index()
+   
+           public class HomeController : Controller
         {
-            return View();
+        IRepository<Question> context;
+            IRepository<QuestionCategory> CategoryContext;
+            IRepository<Answer> AnswerContext;
+
+        public HomeController()
+            {
+                context = new SQLRepository<Question>(new MyContext());
+                CategoryContext = new SQLRepository<QuestionCategory>(new MyContext());
+                AnswerContext = new SQLRepository<Answer>(new MyContext());
+                //context = new InMemoryRepository<Product>();
+                //CategoryContext = new InMemoryRepository<ProductCategory>();
+
+            
+            }
+
+            public HomeController(IRepository<Question> context, IRepository<QuestionCategory> categoryContext)
+            {
+                this.context = context;
+                CategoryContext = categoryContext;
+            }
+
+                              //Parameter(string Category = null)
+            public ActionResult Index()
+            {
+                // List<Question> questions;
+                // List<QuestionCategory> categories = CategoryContext.Collection().ToList();
+                // if (Category == null)
+                // {
+                //     questions = context.Collection().ToList();
+                // }
+                // else
+                // {
+                //     questions = context.Collection().Where(p => p.Category == Category).ToList();
+                // }
+                //
+                // QuestionListViewModel viewModel = new QuestionListViewModel();
+                // viewModel.Questions = questions;
+                // viewModel.QuestionCategories = categories;
+                //
+                // return View(viewModel);
+                return View();
+            }
+         
+           public ActionResult QuestionByCategory(string Category = null, string Question = null)
+            {
+                List<Question> questions;
+                List<QuestionCategory> categories = CategoryContext.Collection().ToList();
+                List<Answer> answers = AnswerContext.Collection().ToList();
+                
+                if (Category == null)
+                {
+                    questions = context.Collection().ToList();
+                   
+                }
+                else
+                {
+                    questions = context.Collection().Where(p => p.Category == Category).ToList();
+                    if (Question != null)
+                    {
+                        answers = AnswerContext.Collection().Where(a =>a.QuestionObj == Question).ToList();
+                    }
+                }
+
+
+
+                QuestionListViewModel viewModel = new QuestionListViewModel();
+                viewModel.Questions = questions;
+                viewModel.QuestionCategories = categories;
+                viewModel.Answers = answers;
+
+                return View(viewModel);
+            }
+          
+            public ActionResult Contact()
+            {
+                ViewBag.Message = "Your contact page.";
+
+                return View();
+            }
         }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
-    }
 }
